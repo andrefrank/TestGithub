23d088b520aaadf612f195dfb1dcc5b56d2b6e3f
Andre Frank <andrefrank@macbookpro1.speedport_w_921v_1_44_000>
2018-10-21 00:16:16 +0200

    Added MapForIternaty

---
+++ TestGithub/MapForIternaty.swift
@@ -0,0 +1,13 @@
+//
+//  MapForIternaty.swift
+//  TestGithub
+//
+//  Created by Andre Frank on 21.10.18.
+//  Copyright © 2018 Afapps+. All rights reserved.
+//
+
+import UIKit
+
+class MapForIternaty: NSObject {
+
+}
